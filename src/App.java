a5e31580215f29aeb876df0c027bb6d1e2ae6c65
thiago-laza <thiago.profmat@gmail.com>
2024-06-18 20:32:33 -0300

    Solicitacao do nome do usuario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite seu nome completo: ");
+        String nome = sc.nextLine();
+        sc.close();
+
+        System.out.println("Bem vindo, " + nome + "!");
     }
 }
